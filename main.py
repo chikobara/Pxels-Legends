0700e1c60ee4da218ea3944fbe613140f16068c2
Chikobara <anonymoussyria6@gmail.com>
2022-08-30 01:34:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-from turtle import width
 import pygame as pg
 
+fps = 60
 width , height = 800, 600
 win = pg.display.set_mode((width, height))
 pg.display.set_caption("Brick Breaker")
 
+def draw (win):
+    win.fill("WHITE")
+    pg.display.update()
 
+
+def main():
+    clock = pg.time.Clock()
+    run = 1
+
+    while run:
+        clock.tick(fps)
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                run = 0
+                break
+        draw(win)
+    
+    pg.quit()
+    quit()
+
+if __name__ == "__main__":
+    main()    
