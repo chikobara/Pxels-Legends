2f8015810d49ef5ef1566a364db72d454b4b48e4
Chikobara <anonymoussyria6@gmail.com>
2022-08-30 12:41:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,30 @@
 import pygame as pg
 
+pg.init()
+
+
 fps = 60
-WIDTH, HEIGHT = 800, 600
-win = pg.display.set_mode((WIDTH, HEIGHT))
-pg.display.set_caption("Brick Breaker")
-PADDLE_WIDTH = 40
-PADDLE_HEIGHT = 10
+
+# window :
+WW, WH = 800, 400
+win = pg.display.set_mode((WW, WH))
+pg.display.set_caption("Pixels-Legends")
 
 
-class Paddle:
-    VEL = 5
-
-    def __init__(self, x, y, width, height, color):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
-
-    def draw(self, win):
-        pg.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
-
-
-def draw(win):
-    win.fill("WHITE")
-    pg.display.update()
+def draw():
+    pass
 
 
 def main():
-    clock = pg.time.Clock()
     run = 1
-    paddle = Paddle(WIDTH / 2, HEIGHT / 2)
 
     while run:
-        clock.tick(fps)
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 run = 0
                 break
-        draw(win)
 
     pg.quit()
-    quit()
 
 
 if __name__ == "__main__":
